26a2a34c886a7a7610430abfc5f995d4c88f9b98
Mayke Alisson <mayke.furtado@arcom.com.br>
2020-11-17 17:53:55 -0300

    feat: criando home

---
+++ app/src/main/java/br/com/redesenhe/protect/view/CustomDialogNovoGrupo.java
@@ -0,0 +1,64 @@
+package br.com.redesenhe.protect.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import br.com.redesenhe.protect.R;
+
+public class CustomDialogNovoGrupo extends AppCompatDialogFragment {
+    private EditText editTextNomeGrupo;
+    private CustomDialogListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog, null);
+        builder.setView(view)
+                .setTitle("Adicionar Grupo")
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String nomeGrupo = editTextNomeGrupo.getText().toString();
+                        listener.applyText(nomeGrupo);
+                    }
+                });
+        editTextNomeGrupo = view.findViewById(R.id.layout_dialog_nome_grupo);
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (CustomDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    "Tem que implementar CustomDialogNovoGrupo");
+        }
+
+    }
+
+    public interface CustomDialogListener{
+        void applyText(String nomeGrupo);
+    }
+}
--- app/src/main/java/br/com/redesenhe/protect/view/HomeActivity.kt
+++ app/src/main/java/br/com/redesenhe/protect/view/HomeActivity.kt
@@ -1,13 +1,18 @@
 package br.com.redesenhe.protect.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import br.com.redesenhe.protect.R
 import br.com.redesenhe.protect.viewmodel.HomeViewModel
-import br.com.redesenhe.protect.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener,
+    CustomDialogNovoGrupo.CustomDialogListener {
 
     private lateinit var mViewModel: HomeViewModel
 
@@ -23,6 +28,58 @@ class HomeActivity : AppCompatActivity() {
             title = "Home"
         }
 
-
+        // Inicializa eventos
+        setListeners()
+        observe()
     }
+
+    @Override
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @Override
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_home_fechar -> {
+                Toast.makeText(this, "Menu Fechar", Toast.LENGTH_LONG).show()
+            }
+            R.id.menu_home_mudar_senha -> {
+                Toast.makeText(this, "Menu Mudar senha master", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.activity_home_btnAdicionarGrupo -> {
+                openDialog()
+            }
+        }
+    }
+
+    fun openDialog(){
+        val customDialogNovoGrupo = CustomDialogNovoGrupo()
+        customDialogNovoGrupo.show(supportFragmentManager, "Adicionar Grupo")
+    }
+
+    override fun applyText(nomeGrupo: String) {
+        Toast.makeText(this, "Nome do Grupo $nomeGrupo", Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * Inicializa os eventos de click
+     */
+    private fun setListeners() {
+        activity_home_btnAdicionarGrupo.setOnClickListener(this)
+    }
+
+    /**
+     * Observa ViewModel
+     */
+    private fun observe() {}
+
 }
